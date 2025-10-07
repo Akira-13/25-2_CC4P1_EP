d342f52f2b0b4e1b349ef390b8e162f98ad8c53d
Diego Akira García Rojas <ak13a@tutamail.com>
2025-10-07 11:57:41 -0500

    Estructura inicial

---
+++ src/main/java/cc4p1/cc4p1_ep/CC4P1_EP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cc4p1.cc4p1_ep;
+
+/**
+ *
+ * @author ak13a
+ */
+public class CC4P1_EP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
